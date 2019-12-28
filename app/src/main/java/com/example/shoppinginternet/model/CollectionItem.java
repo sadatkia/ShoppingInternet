8dcd9a0a6a531be29f9cdf44b15bbd53afcd7516
Task2 <sadat>
2019-12-28 15:52:07 +0330

    recycelerView horisental and check for it's implimention

---
+++ app/src/main/java/com/example/shoppinginternet/model/CollectionItem.java
@@ -0,0 +1,26 @@
+package com.example.shoppinginternet.model;
+
+
+import com.google.gson.annotations.SerializedName;
+
+public class CollectionItem{
+
+	@SerializedName("href")
+	private String href;
+
+	public void setHref(String href){
+		this.href = href;
+	}
+
+	public String getHref(){
+		return href;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"CollectionItem{" + 
+			"href = '" + href + '\'' + 
+			"}";
+		}
+}
